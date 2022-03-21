ee1017e175cceb177da0a8e55946a7b31b450663
Vincent Post <cent@spline.de>
2022-03-21 17:45:40 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SteamAuthTicket
+
+This is just a quick tool to generate a SteamAuthTicket for an appid and write the base64 representation to a file.
---
+++ SteamAuthTicket/Program.cs
@@ -0,0 +1,11 @@
+﻿using Steamworks;
+
+uint appId = Convert.ToUInt32(Environment.GetCommandLineArgs()[1]);
+string path = Environment.GetCommandLineArgs()[2];
+
+File.WriteAllText("steam_appid.txt", Convert.ToString(appId));
+
+SteamClient.Init(39210);
+byte[] ticket = SteamUser.GetAuthSessionTicket().Data;
+
+File.WriteAllText(path, Convert.ToBase64String(ticket));
