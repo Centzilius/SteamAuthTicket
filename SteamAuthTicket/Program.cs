cd722e2f7c19cc42dc4c49396d3682a6f26ce7fa
Vincent Post <cent@spline.de>
2022-03-27 18:05:01 +0200

    fix: whitespaces in path work now

--- SteamAuthTicket/Program.cs
+++ SteamAuthTicket/Program.cs
@@ -6,14 +6,11 @@ uint appId = Convert.ToUInt32(Environment.GetCommandLineArgs()[1]);
 string gameExec = Environment.GetCommandLineArgs()[2];
 IEnumerable<String> gameArgs = Environment.GetCommandLineArgs().Skip(3);
 
-string gameArgsString = string.Join(" ", gameArgs);
-
 File.WriteAllText("steam_appid.txt", Convert.ToString(appId));
 
 SteamClient.Init(appId);
 byte[] ticket = SteamUser.GetAuthSessionTicket().Data;
 string ticketArg = Convert.ToBase64String(ticket);
 
-Process game = Process.Start(gameExec, gameArgsString + " --steamticket=" + ticketArg);
-
+Process game = Process.Start(gameExec, gameArgs.Append("--steamticket=" + ticketArg));
 Thread.Sleep(1000*60*10);
